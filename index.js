2a30e0bfd26d2ec0ba90ff4e977855e3ac9be008
FrierenDv <hackerxct007@gmail.com>
2024-05-26 21:34:49 +0700

    TODO: FULLY REWRITE ENTIRE SOURCE CODE

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// TODO: FULLY REWRITE ENTIRE SOURCE CODE
+
 import "dotenv/config";
 
 import { bot } from "./lib/connection.js";
@@ -8,10 +10,7 @@ await commands("./commands", bot);
 bot.launch();
 bot.command("start", async (ctx) => {
 	await ctx.sendMessage(
-		`Hi, I'm *BOT* named Rose, but not the famous *BOT* you know.
-I can make your photo looks better.
-You can use me by send me a photo or use these commands:
-/start - Show this message
+		`/start - Show this message
 /remini - Make your photo looks better
 /deep_fake - Make your photo looks like a video
 /turnme - Make your photo looks like a cartoon
